8f3082052e9730652a923e12a6c8eb7605b88d2f
Pancakem <pancakesdeath@protonmail.com>
2019-11-22 22:20:09 +0300

    Start building game entities

---
+++ entity.py
@@ -0,0 +1,23 @@
+import pygame
+
+START_POS = (5, 6)
+
+# Entity represents the characters in the game
+class Entity(object):
+
+    def __init__(self, pos):
+        pass       
+
+    def evolve(self):
+        pass
+
+    def explode(self):
+        pass
+
+    def move(self):
+        pass
+
+    def update(self):
+        pass
+    
+        
